3a5370f3d801be67dec90d887e6bdcefc3605c4b
dAnIeLdK03 <118935791+dAnIeLdK03@users.noreply.github.com>
2025-06-10 23:51:39 +0300

    Delete HotelReservationSystem/HotelReservationSystem/HotelSystemManegment directory

--- HotelReservationSystem/HotelReservationSystem/HotelSystemManegment/src/main/java/com/hotelsystemmanegment/Repositories/RoomImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelsystemmanegment.Repositories;
-
-import com.hotelsystemmanegment.Entity.RoomImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
-} 
